64049fe8e3dfbf21190692ce36f0306fe30a9e22
Filipe Santiago <porto.santiago@hotmail.com>
2021-04-24 10:46:19 -0300

    add client schema

---
+++ src/client/client.schema.ts
@@ -0,0 +1,25 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { City, CitySchema } from '../city/city.schema';
+import { Entity } from '../shared/utils/entity';
+import { Sex } from './types';
+
+@Schema({ timestamps: true })
+export class Client extends Entity {
+    @Prop({ required: true })
+    public name: string;
+
+    @Prop({ type: Sex, enum: [Sex.masculine, Sex.feminine], required: true })
+    public sex: Sex;
+
+    @Prop({ required: true })
+    public age: number;
+
+    @Prop({ required: true })
+    public birthDate: Date;
+
+    @Prop({ type: CitySchema })
+    public city: City;
+}
+
+export type ClientDocument = Client & Document;
+export const ClientSchema = SchemaFactory.createForClass(Client);
